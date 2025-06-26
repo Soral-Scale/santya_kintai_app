943b68412181e8b74003698ba75af1ad164a051d
Soral-Scale <moonlightcookie2145@gmail.com>
2025-06-26 19:36:19 +0900

    Initial commit

--- app.py
+++ app.py
@@ -211,5 +211,4 @@ def get_all_notices():
 
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=5000, debug=True)
